55906889aeabef4eb06b0a4b4595908967197713
larybino <binolaryssa@gmail.com>
2024-04-29 11:51:56 -0300

    third commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -17,9 +17,8 @@ public class ContaTerminal {
 
         System.out.println("Por favor, digite o saldo:");
         double saldo = scanner.nextDouble();
-
+        
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
-
         scanner.close(); 
     }
 }
